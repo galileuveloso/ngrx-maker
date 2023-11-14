deb7425624cec3a85abb047354f3e7a5e058eeb5
galileu.veloso <galileu.veloso@mps.com.br>
2023-11-14 16:50:01 -0300

    set icon

--- NgRxMaker/NgRxMaker/Forms/NgRxMaker.Designer.cs
+++ NgRxMaker/NgRxMaker/Forms/NgRxMaker.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NgRxMaker));
             this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.BrowseFolderBtn = new System.Windows.Forms.Button();
             this.CloseBtn = new System.Windows.Forms.Button();
@@ -38,9 +39,10 @@
             // 
             // BrowseFolderBtn
             // 
-            this.BrowseFolderBtn.Location = new System.Drawing.Point(12, 73);
+            this.BrowseFolderBtn.Location = new System.Drawing.Point(9, 59);
+            this.BrowseFolderBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BrowseFolderBtn.Name = "BrowseFolderBtn";
-            this.BrowseFolderBtn.Size = new System.Drawing.Size(280, 26);
+            this.BrowseFolderBtn.Size = new System.Drawing.Size(210, 21);
             this.BrowseFolderBtn.TabIndex = 1;
             this.BrowseFolderBtn.Text = "Create Feature";
             this.BrowseFolderBtn.UseVisualStyleBackColor = true;
@@ -48,9 +50,10 @@
             // 
             // CloseBtn
             // 
-            this.CloseBtn.Location = new System.Drawing.Point(12, 115);
+            this.CloseBtn.Location = new System.Drawing.Point(9, 93);
+            this.CloseBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CloseBtn.Name = "CloseBtn";
-            this.CloseBtn.Size = new System.Drawing.Size(280, 23);
+            this.CloseBtn.Size = new System.Drawing.Size(210, 19);
             this.CloseBtn.TabIndex = 2;
             this.CloseBtn.Text = "Close";
             this.CloseBtn.UseVisualStyleBackColor = true;
@@ -59,38 +62,43 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Location = new System.Drawing.Point(7, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Entity";
             // 
             // EntityTxt
             // 
-            this.EntityTxt.Location = new System.Drawing.Point(12, 32);
+            this.EntityTxt.Location = new System.Drawing.Point(9, 26);
+            this.EntityTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EntityTxt.Name = "EntityTxt";
-            this.EntityTxt.Size = new System.Drawing.Size(280, 22);
+            this.EntityTxt.Size = new System.Drawing.Size(211, 20);
             this.EntityTxt.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 153);
+            this.label2.Location = new System.Drawing.Point(7, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.Size = new System.Drawing.Size(108, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "@Galileu Frez Veloso";
             // 
             // NgRxMaker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 179);
+            this.ClientSize = new System.Drawing.Size(228, 145);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.EntityTxt);
             this.Controls.Add(this.CloseBtn);
             this.Controls.Add(this.BrowseFolderBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "NgRxMaker";
             this.Text = "NgRxMaker";
             this.ResumeLayout(false);
